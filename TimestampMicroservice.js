1988d5458e0aef9bfac04cfc07d0dd5bdd24d2ff
Nikhil Malhotra <nikhilmalhotra.0111@gmail.com>
2020-09-19 18:33:43 +0530

    Create TimestampMicroservice.js

---
+++ TimestampMicroservice.js
@@ -0,0 +1,21 @@
+app.get("/api/timestamp/:date_string",function(req,res){
+  let dateString = req.params.date_string;
+  if(/\d{5,}/.test(dateString)){
+    let dateInt=parseInt(dateString);
+    let dateObj1=new Date(dateInt);
+    if(dateObj1.toString()==="Invalid Date"){
+        res.json({"error" : "Invalid Date"});
+    }
+    else{}
+    res.json({unix:dateString, utc: new Date(dateInt).toUTCString()});
+  }
+
+    let dateObj=new Date(dateString);
+    if(dateObj.toString()==="Invalid Date"){
+        res.json({"error" : "Invalid Date"});
+    }
+    else{
+      res.json({unix: dateObj.valueOf(), utc: dateObj.toUTCString()});
+    }
+  
+});
